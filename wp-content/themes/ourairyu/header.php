5ebd882307a8408fbbf21b12445565fbfc93891f
Ourai Lin <ourairyu@hotmail.com>
2015-08-22 00:00:11 +0800

    添加部分 ourairyu 主题的文件

---
+++ wp-content/themes/ourairyu/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <!-- 页面渲染兼容性 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <!-- 站点信息 -->
+    <title>{{ page.special_title }}</title>
+    <meta name="keywords" content="{{ keywords }}">
+    <meta name="description" content="{{ description | strip_html }}">
+  </head>
+  <body itemscope itemtype="http://schema.org/WebPage">
